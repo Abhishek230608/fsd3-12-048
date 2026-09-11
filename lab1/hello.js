87c9f30660382a0ed9f290f99e49fd24d52743a6
Abhishek Pandey <abhishek.25b01010377@abes.ac.in>
2026-09-11 14:25:59 +0530

    lab1

--- lab1/hello.js
+++ lab1/hello.js
@@ -1,16 +1,17 @@
-// console.log("Abhishek Pandey")
+console.log("Dhaneshwar Kumar");
 
-//write a function to take two number and return their sum
+// write a function to take two number and return
+// their sum
 
-function sum(a,b){
-    return a + b;
+function sum(a, b) {
+  return a + b;
 }
-const add = (a,b) => {
-    return a + b;
-};
-const add2 = (a,b) => a + b;
 
-console.log(sum(20,10));
+const add = (a, b) => {
+  return a + b;
+};
+const add2 = (a, b) => a + b;
+
+console.log(sum(20, 10));
 console.log(add(20, 10));
 console.log(add2(20, 10));
-// php
