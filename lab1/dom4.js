df89d944b11dbc364924a784b75397a7180c334a
Abhishek Pandey <abhishek.25b01010377@abes.ac.in>
2026-09-18 14:46:18 +0530

    dom 4 is updated

--- lab1/dom4.js
+++ lab1/dom4.js
@@ -3,6 +3,6 @@ const form = new EventEmitter();
 form.on("submit", (uname, password) => {
   console.log("form submitted");
   console.log(`user name:${uname}`);
-  console.log(`user password:${password}`);
+  console.log(`user's  password:${password}`);
 });
 form.emit("submit", "abc@abc.com 😘", 123321);
