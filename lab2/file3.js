611cbba5931b0917c46ed531972c0c81af6aa9b8
Abhishek Pandey <abhishek.25b01010377@abes.ac.in>
2026-09-21 08:56:26 +0530

    file3 updated

--- lab2/file3.js
+++ lab2/file3.js
@@ -4,7 +4,7 @@ import { stat } from "fs/promises";
 const stat = await stat("file1.js");
 console.log("file size", fstat.size, "bytes");
 console.log(`is file:${fstat.isFile()}`);
-console.log(`is folder:${fstat.isFolder()}`);
+console.log(`iss folder:${fstat.isFolder()}`);
 console.log(`is syslink:${fstat.isSymbolicLink()}`);
 console.log(`created on: ${fstat.birthtime}`);
 console.log(`lastused :${fstat.atime}`);
